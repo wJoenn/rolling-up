4ef99dd9edc4c214051dd009250d4ae52a1f4c2b
Louis Ramos <louisramosdev@gmail.com>
2023-05-12 19:03:03 +0200

    Created interfaces for User

---
+++ client/src/types/ApiReponseUser.ts
@@ -0,0 +1,8 @@
+interface ApiResponseUser {
+  id: number
+  email: string
+  created_at: string | Date
+  updated_at: string | Date
+}
+
+export default ApiResponseUser
---
+++ client/src/types/User.ts
@@ -0,0 +1,8 @@
+interface User {
+  id: number | null
+  email: string | null
+  created_at: Date | null
+  updated_at: Date | null
+}
+
+export default User
