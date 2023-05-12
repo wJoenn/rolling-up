9a2e7fce901bb532a8da86af60cb2f074e1f9caa
Louis Ramos <louisramosdev@gmail.com>
2023-05-12 19:01:13 +0200

    Created UserForm component

---
+++ client/src/components/UserForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <button @click="hasAccount = true">Login</button>
+      <button @click="hasAccount = false">Signup</button>
+    </div>
+
+    <SessionsNew v-if="hasAccount" />
+    <RegistrationsNew v-else />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from "vue"
+  import SessionsNew from "./SessionsNew.vue"
+  import RegistrationsNew from "./RegistrationsNew.vue"
+
+  const hasAccount = ref(true)
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,32 +1,15 @@
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="../assets/images/vite.svg" class="logo" alt="Vite logo">
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="../assets/images/vue.svg" class="logo vue" alt="Vue logo">
-    </a>
+  <div id="home">
+    <h1>Rolling-up</h1>
+    <h3>The best DnD character creation assistant</h3>
+
+    <UserForm />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from "../components/HelloWorld.vue"
+  import UserForm from "../components/UserForm.vue"
 </script>
 
 <style scoped lang="scss">
-  .logo {
-    height: 10em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-
-    &:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-
-    &.vue:hover {
-      filter: drop-shadow(0 0 2em #42b883aa);
-    }
-  }
 </style>
