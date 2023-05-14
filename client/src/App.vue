e8c368ce8fd8f44c21d9380db2dd00f7ff8ee3b4
Louis Ramos <louisramosdev@gmail.com>
2023-05-14 18:04:37 +0200

    Homepage inc

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,10 +4,6 @@
 
 <style lang="scss">
   #app {
-    align-items: center;
-    display: flex;
-    flex-direction: column;
     flex-grow: 1;
-    justify-content: center;
   }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,53 @@
 <template>
-  <div id="home">
-    <div class="banner">
-      <div class="container">
-        <h1>Rolling-up</h1>
-        <h3>The best DnD character creation assistant</h3>
+  <div id="home" class="container">
+    <div />
 
-        <UserForm />
-      </div>
+    <div>
+      <img src="../assets/images/dice.gif" alt="dice">
+      <h1>Rollingup</h1>
+      <p>Welcome to the best DnD character creation assistant. Create an account and </p>
     </div>
+
+    <div class="buttons">
+      <button>Login</button>
+      <button>Signup</button>
+    </div>
+
+    <!-- <UserForm /> -->
   </div>
 </template>
 
 <script setup lang="ts">
-  import UserForm from "../components/UserForm.vue"
+  // import UserForm from "../components/UserForm.vue"
 </script>
 
 <style scoped lang="scss">
+#home {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  min-height: 100svh;
+  text-align: center;
+
+  h1 {
+    font-size: 60px;
+    margin-bottom: 20px;
+  }
+
+  img {
+    margin-bottom: -50px;
+  }
+
+  .buttons {
+    display: flex;
+    flex-direction: column;
+    font-weight: 700;
+    gap: 20px;
+
+    button {
+      font: inherit;
+      padding: $padding;
+    }
+  }
+}
 </style>
