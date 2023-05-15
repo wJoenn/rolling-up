a1c2a78a78f2af96f7a530549455e32a9c80d7e1
Louis Ramos <louisramosdev@gmail.com>
2023-05-15 16:55:28 +0200

    Animated home dice

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,9 +3,15 @@
     <div />
 
     <div>
-      <img src="../assets/images/dice.gif" alt="dice">
-      <h1>Rollingup</h1>
-      <p>Welcome to the best DnD character creation assistant. Create an account and access your characters anytime.</p>
+      <div class="gif"><div /></div>
+
+      <div class="title">
+        <h1>Rollingup</h1>
+        <p>
+          Welcome to the best DnD character creation assistant.
+          Create an account and access your characters anytime.
+        </p>
+      </div>
     </div>
 
     <div class="buttons">
@@ -26,6 +32,46 @@
 </script>
 
 <style scoped lang="scss">
+@keyframes jump {
+  0% {
+    // bottom: 0;
+    // translate: 0 0;
+    // margin-bottom: -50px;
+    transform: translate3d(0, 0, 0);
+  }
+
+  50% {
+    // bottom: 10px;
+    // translate: 0 -3%;
+    // margin-bottom: -40px;
+    transform: translate3d(0, -3%, 0);
+  }
+
+  100% {
+    // bottom: 0;
+    // translate: 0 0;
+    // margin-bottom: -50px;
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+@keyframes pulse-rotate {
+  0% {
+    rotate: 0deg;
+    scale: 1;
+  }
+
+  50% {
+    rotate: 180deg;
+    scale: 0.98;
+  }
+
+  100% {
+    rotate: 360deg;
+    scale: 1;
+  }
+}
+
 #home {
   display: flex;
   flex-direction: column;
@@ -38,15 +84,42 @@
     margin-bottom: 20px;
   }
 
-  img {
-    margin-bottom: -50px;
-  }
-
   .buttons {
     display: flex;
     flex-direction: column;
     font-weight: 700;
     gap: 20px;
+  }
+
+  .gif {
+    animation: jump 2s linear infinite;
+
+    div {
+      aspect-ratio: 1/1;
+      background-image: url("../assets/images/dice.png");
+      background-size: cover;
+      width: 100%;
+      position: relative;
+
+      &::before {
+        animation: pulse-rotate 10s linear infinite;
+        background-image: linear-gradient(225deg, rgb(68, 208, 243), rgb(204, 52, 204));
+        border-radius: 50%;
+        content: "";
+        filter: blur(20px);
+        height: 100%;
+        opacity: 0.3;
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 100%;
+        z-index: -10;
+      }
+    }
+  }
+
+  .title {
+    translate: 0 -30px;
   }
 }
 </style>
