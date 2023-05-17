08cb10d54182ee9c540f9f3268d07fe7f010fad1
Louis Ramos <louisramosdev@gmail.com>
2023-05-17 22:12:54 +0200

    Created CharactersController#create action

--- api/app/controllers/characters_controller.rb
+++ api/app/controllers/characters_controller.rb
@@ -1,10 +1,32 @@
 class CharactersController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: %i[total]
+
+  before_action :character_params, only: %i[create]
+
+  def create
+    character = current_user.characters.new(character_params)
+
+    if character.save
+      render json: {
+        message: "Character created successfully.",
+        character:
+      }, status: :ok
+    else
+      render json: {
+        message: "Character couldn't be created successfully.",
+        errors: character.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
 
   def total
-    @total = Character.last&.id || 0
-    render json: {
-      total: @total
-    }, status: :ok
+    total = Character.last&.id || 0
+    render json: { total: }, status: :ok
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name)
   end
 end
--- api/spec/controllers/characters_controller_spec.rb
+++ api/spec/controllers/characters_controller_spec.rb
@@ -1,21 +1,62 @@
 require "rails_helper"
 
 RSpec.describe CharactersController, type: :request do
+  let!(:user) { User.create(email: "user@example.com", password: "password") }
+  let!(:params) { { name: "Joenn" } }
+
+  describe "POST /create" do
+    before do
+      sign_in user
+      post "/characters", params: { character: params }
+    end
+
+    it "requires to be logged in" do
+      sign_out user
+      post "/characters", params: { character: params }
+
+      expect(response).to have_http_status :found
+    end
+
+    it_behaves_like "a JSON object"
+
+    it "returns http success with proper params" do
+      expect(response).to have_http_status :success
+    end
+
+    it "created a new Character with proper params" do
+      expect(Character.exists?(name: "Joenn")).to be_truthy
+    end
+
+    it "creates a new Character with proper params" do
+      expect(Character.exists?(name: "Joenn")).to be_truthy
+      expect(response).to have_http_status :success
+      expect(response.parsed_body["message"]).to eq "Character created successfully."
+    end
+
+    it "returns an error message with incorrect params" do
+      sign_in user
+      post "/characters", params: { character: { name: nil } }
+
+      expect(response).to have_http_status :unprocessable_entity
+      expect(response.parsed_body["message"]).to eq "Character couldn't be created successfully."
+      expect(response.parsed_body["errors"].any?).to be_truthy
+    end
+  end
+
   describe "GET /total" do
     before do
       get "/characters/total"
     end
 
+    it_behaves_like "a JSON object"
+
     it "returns http success" do
       expect(response).to have_http_status :success
     end
 
-    it_behaves_like "a JSON object"
-
     it "returns a total as an integer" do
       expect(response.parsed_body["total"]).to be_zero
 
-      user = User.create(email: "user@example.com", password: "password")
       character = Character.create(name: "Joenn", user:)
       get "/characters/total"
 
